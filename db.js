77fc660a2262e511c2da167b216f140ce2b32360
victor <victor.charpenay@insa-lyon.fr>
2015-04-04 22:26:38 +0200

    DB binding done.

--- db.js
+++ db.js
@@ -2,22 +2,60 @@
  * Database access
  */
 var sqlite3 = require('sqlite3');
+var events = require('events');
 
 var db = new sqlite3.Database('dateup_db');
+var emitter = new events.EventEmitter();
 
-function interests() {
-	db.serialize(function() {
-		db.all('SELECT * FROM table WHERE something', function(err, rows) {
-			if (err) {
-				console.error('error: ' + err);
-			} else {
-				// process data
-			}
-		});
-	});
+/**
+ * 
+ * @param userID
+ * @param cb cb('info', [id], {firstname, lastname, age})
+ */
+function info(userID, cb) {
+	query('SELECT firstname, lastname, age FROM person WHERE id=?', [userID], cb, 'info', [userID]);
+}
+
+/**
+ * 
+ * @param userID
+ * @param cb cb('interests', [id], ["interest1", "interest2", ...])
+ */
+function interests(userID, cb) {
+	queryall('SELECT i.label AS label FROM interest AS i, person_interest AS pi WHERE i.id=pi.interest_id AND pi.person_id=?', [userID], cb, 'interests', [userID]);
+}
+
+/**
+ * 
+ * @param userID
+ * @param cb cb('dateups', [id], [{interest, date}, ...])
+ */
+function dateups(userID, cb) {
+	queryall('SELECT i.label AS label, d.date AS date FROM interest AS i, dateup as d WHERE i.id=d.interest AND (d.person_a=? OR d.person_b=?)', [userID, userID], cb, 'dateups', [userID]);
+}
+
+function process(cb, event, params) {
+	return function(err, result) {
+		if (err) {
+			console.error('[db.js] - ' + err);
+			cb(event, params, null);
+		} else {
+			cb(event, params, result);
+		}
+	}
+}
+
+function query(sql, params, callback, event, params) {
+	db.get(sql, params, process(callback, event, params));
+}
+
+function queryall(sql, params, callback, event, params) {
+	db.all(sql, params, process(callback, event, params));
 }
 
 /**
  * module interface
  */
 exports.interests = interests;
+exports.info = info;
+exports.dateups = dateups;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require('express');
 var bp = require('body-parser');
 var app = express();
 
+var db = require('./db.js');
+
 //configures a module that automatically serve data in /public
 app.use(express.static(__dirname + '/public'));
 //configures a module to populate prop request's body
@@ -19,14 +21,43 @@ app.get('/', function(req, res) {
 });
 
 app.get('/dashboard', function(req, res) {
-	var data = {
-		user: {
-			name: 'John Doe',
-			interests: ['cooking'],
-			dateups: []
-		}
+	var id = req.query.id;
+	var cb = function(data, events) {
+		return function(event, params, ret) {
+			switch (event) {
+				case 'info':
+					data.user.name = ret.firstname + ' ' + ret.lastname;
+					data.user.age = ret.age;
+					break;
+				case 'interests':
+					data.user.interests = ret;
+					break;
+				case 'dateups':
+					data.user.dateups = ret;
+					break;
+			}
+			events[event] = true;
+			var completed = true;
+			for (k in events) {
+				completed &= events[k];
+			}
+			if (completed) {
+				res.render('dashboard', data);
+				console.log(JSON.stringify(data));
+			}
+		};
 	};
-	res.render('dashboard', data);
+	var data = {
+		user: {}	
+	};
+	var events = {
+		'info': false,
+		'interests': false,
+		'dateups': false
+	}
+	db.info(id, cb(data, events));
+	db.interests(id, cb(data, events));
+	db.dateups(id, cb(data, events));
 });
 
 app.listen(8069);
