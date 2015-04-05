f8e0a618d69c1f030130665e73c3ec9a01015600
victor <victor.charpenay@insa-lyon.fr>
2015-04-05 14:21:17 +0200

    switched backend to deployd.

--- server.js
+++ server.js
@@ -1,73 +1,21 @@
 /**
  * Main server file
  */
-var express = require('express');
-var bp = require('body-parser');
-var app = express();
+var dpd = require('deployd');
 
-var db = require('./db.js');
+var options = {
+	port: 8069,
+	db: {
+		port: 27017,
+		host: 'localhost',
+		name: 'dateup'
+	},
+	env: 'development'
+};
 
-//configures a module that automatically serve data in /public
-app.use(express.static(__dirname + '/public'));
-//configures a module to populate prop request's body
-app.use(bp());
-//configures Jade template engine to work with Express
-app.set('view engine', 'jade');
-app.engine('jade', require('jade').__express);
+var server = dpd(options);
 
-//sets primary route
-app.get('/', function(req, res) {
-	res.sendfile(__dirname + '/public/index.html');
+server.listen();
+server.on('listening', function() {
+	console.log('listening on port ' + server.options.port + '...');
 });
-
-app.get('/dashboard', function(req, res) {
-	var id = req.query.id;
-	var data = {
-		user: {}
-	};
-	var next = function(fct, param, cb) {
-		fct(param, function(event, params, ret) {
-			cb(event, params, ret);
-		});
-	};
-	db.info(id, function(event, params, ret) {
-		
-	});
-	var cb = function(data, events) {
-		return function(event, params, ret) {
-			switch (event) {
-				case 'info':
-					data.user.name = ret.firstname + ' ' + ret.lastname;
-					data.user.age = ret.age;
-					data.user.profilepic = ret.pic;
-					break;
-				case 'interests':
-					data.user.interests = ret;
-					break;
-				case 'dateups':
-					data.user.dateups = ret;
-					break;
-			}
-			events[event] = true;
-			var completed = true;
-			for (k in events) {
-				completed &= events[k];
-			}
-			if (completed) {
-				res.render('dashboard', data);
-				console.log(JSON.stringify(data));
-			}
-		};
-	};
-	var events = {
-		'info': false,
-		'interests': false,
-		'dateups': false
-	}
-	db.info(id, cb(data, events));
-	db.interests(id, cb(data, events));
-	db.dateups(id, cb(data, events));
-});
-
-app.listen(8069);
-console.log('listening on port 8069...');
