3d1d6d1717f75737712bdb9ac9fd050c3fed1580
victor <victor.charpenay@insa-lyon.fr>
2015-04-05 17:17:20 +0200

    tried to restore development config

--- config.js
+++ config.js
@@ -1,21 +1,13 @@
 /**
  * Deployment configuration
- * TODO list:
- *  - create non-admin user
- *  - catch signals
- *  - env production instead of dev
  */
 var config = {
-	host: process.env.OPENSHIFT_NODEJS_IP || 'localhost',
-	port: process.env.OPENSHIFT_NODEJS_PORT || 8069,
+	host: 'localhost',
+	port: 8069,
 	db: {
-		host: process.env.OPENSHIFT_MONGODB_DB_HOST,
-		port: process.env.OPENSHIFT_MONGODB_DB_PORT,
-		name: 'dateup',
-		credentials: {
-			username: 'admin',
-			password: 'ahwKm_NvX3Ws'
-		}
+		host: 'localhost',
+		port: 27017,
+		name: 'dateup'
 	},
 	env: 'development'
 }
