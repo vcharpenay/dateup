904036198371888c472f1a51ce5af1047915b1a2
Worathiti Manosroi <w.manosroi@gmail.com>
2015-04-06 23:39:31 +0200

    facebook login ...

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,30 @@ app.controller('MainCtrl', function ($scope, ngDialog) {
             showClose : false
         });
     };
+    
+    $scope.facebookLogin = function(){
+        FB.login(function(response) {
+            //TODO: save user to DB
+          if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+              window.location = 'profile.html';
+          } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+          } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+          }
+
+        }, {scope: 'public_profile,email,user_likes'});
+//        FB.getLoginStatus(function(response) {
+//          if (response.status === 'connected') {
+//            console.log('Logged in.');
+//          }
+//          else {
+//            FB.login();
+//          }
+//        });
+    }
 });
 
 app.controller('FirstDialogCtrl', function ($scope, ngDialog) {
